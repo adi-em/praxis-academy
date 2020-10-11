ab2207a023bd85b39489b7f906cc879d9dbabe05
adi-em <adimurdiono@student.unu-jogja.ac.id>
2020-10-11 15:19:04 +0700

    programan modules

---
+++ kemampuan dasar/novice/01-02/modules/main.py
@@ -0,0 +1,5 @@
+import matematika as jumlah
+
+jumlah.tambah(6,5)
+perkalian = jumlah.perkalian(9,5)
+print(perkalian)
---
+++ kemampuan dasar/novice/01-02/modules/matematika.py
@@ -0,0 +1,8 @@
+def tambah (a,b):
+    print(a,'+',b,'=',a+b)
+    print('penambahan')
+
+def perkalian(a,b):
+    print('perkalian')
+    return a + b
+
