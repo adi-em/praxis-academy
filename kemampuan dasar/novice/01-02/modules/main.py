f94f22fb7f4d2dc62368b3d215d3bbd2975add2f
adi-em <adimurdiono@student.unu-jogja.ac.id>
2020-10-12 13:00:13 +0700

    modules

--- kemampuan dasar/novice/01-02/modules/main.py
+++ kemampuan dasar/novice/01-02/modules/main.py
@@ -3,3 +3,11 @@ import matematika as jumlah
 jumlah.tambah(6,5)
 perkalian = jumlah.perkalian(9,5)
 print(perkalian)
+
+DaftarAngka = [23,7,32,99,4,15,11,20]
+BubbleSort = jumlah.BubbleSort(DaftarAngka)
+print(DaftarAngka)
+
+angka = [73,2,23,43,1,21,11,50]
+SelectionSort = jumlah.SelectionSort(angka)
+print(angka)
--- kemampuan dasar/novice/01-02/modules/matematika.py
+++ kemampuan dasar/novice/01-02/modules/matematika.py
@@ -6,3 +6,24 @@ def perkalian(a,b):
     print('perkalian')
     return a + b
 
+def BubbleSort(val):
+    for namber in range(len(val)-1,0,-1):
+        for i in range(namber):
+            if val[i]>val[i+1]:
+                temp = val[i]
+                val[i] = val[i+1]
+                val[i+1] = temp
+
+def SelectionSort(val):
+   for isi in range(len(val)-1,0,-1):
+       Max=0
+       for lokasi in range(1,isi+1):
+           if val[lokasi]>val[Max]:
+               Max = lokasi
+ 
+       temp = val[isi]
+       val[isi] = val[Max]
+       val[Max] = temp
+ 
+
+
