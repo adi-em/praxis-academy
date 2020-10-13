a71033797b75394a280b3153b96291ab95ce60f4
adi-em <adimurdiono@student.unu-jogja.ac.id>
2020-10-13 13:53:15 +0700

    program loop

---
+++ kemampuan dasar/novice/01-02/Loop.py
@@ -0,0 +1,20 @@
+#perulangan dengan for
+ulang = 10
+
+for i in range(ulang):
+    print ("Perulangan ke-"+str(i))
+
+item = ['kopi','nasi','teh','jeruk']
+
+for isi in item:
+    print (isi)
+
+
+#perulangan dengan while
+
+angka = 1
+while (angka < 9):
+    print ("nomor:", angka)
+    angka = angka + 1
+
+print ("selesai!")
