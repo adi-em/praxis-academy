1a4a9290a3a36e0bed2b1debe5302ef382d21abf
adi-em <adimurdiono@student.unu-jogja.ac.id>
2020-10-13 14:04:58 +0700

    file pemanggil

--- kemampuan dasar/novice/01-02/modules/main.py
+++ kemampuan dasar/novice/01-02/modules/main.py
@@ -11,3 +11,16 @@ print(DaftarAngka)
 angka = [73,2,23,43,1,21,11,50]
 SelectionSort = jumlah.SelectionSort(angka)
 print(angka)
+
+nomor = [56,7,34,65,23,12,5,20]
+InsertionSort = jumlah.InsertionSort(nomor)
+print(nomor)
+
+list = [67,91,87,33,49,10,16,43,65,3]
+print('Data yang akan di sort :', list)
+print('Quick Sort :')
+qs = jumlah.qs(list,0,len(list)-1)
+
+print('Menggabungkan List', list)
+list = [2,5,60,43,27,10,89,17]
+ms = jumlah.ms(list)
